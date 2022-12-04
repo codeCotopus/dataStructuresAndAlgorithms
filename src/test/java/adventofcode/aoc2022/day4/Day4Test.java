8d009edbf3592db3cef560ab854622b2b0a87186
Walter Zimerman <walter.tsp@gmail.com>
2022-12-04 07:15:43 -0800

    Refactors AOC Day4. Removes for loops. Sets total as a field. Removes unnecesary boxing of ints

--- src/test/java/adventofcode/aoc2022/day4/Day4Test.java
+++ src/test/java/adventofcode/aoc2022/day4/Day4Test.java
@@ -1,8 +1,10 @@
 package adventofcode.aoc2022.day4;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -11,44 +13,57 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 public class Day4Test {
+
+    private static int total;
+
+    @BeforeEach
+    void setUp() {
+        total = 0;
+    }
+
     @Test
     public void inputPart1() {
-        assertEquals(431, computePart1(Day4Input.INPUT, new FirstPartDay4Strategy()));
+        assertEquals(431, solve(Day4Input.INPUT, new FirstPartDay4Strategy()));
     }
 
     @Test
     public void samplePart1() {
-        assertEquals(2, computePart1(Day4Input.SAMPLE, new FirstPartDay4Strategy()));
+        assertEquals(2, solve(Day4Input.SAMPLE, new FirstPartDay4Strategy()));
     }
 
     @Test
     public void inputPart2() {
-        assertEquals(823, computePart1(Day4Input.INPUT, new SecondPartDay4Strategy()));
+        assertEquals(823, solve(Day4Input.INPUT, new SecondPartDay4Strategy()));
     }
 
     @Test
     public void samplePart2() {
-        assertEquals(4, computePart1(Day4Input.SAMPLE, new SecondPartDay4Strategy()));
+        assertEquals(4, solve(Day4Input.SAMPLE, new SecondPartDay4Strategy()));
     }
 
-    private static int computePart1(String input, Day4Strategy setOperation) {
+    private static int solve(String input, Day4Strategy setOperation) {
         String[] pairs = input.split("\n");
-        int total = 0;
-        for (String pair : pairs) {
-            String[] sectionRanges = pair.split(",");
-            int elf = 0;
-            int[][] boundaries = new int[2][2];
-            for (String range : sectionRanges) {
-                String[] sections = range.split("-");
-                boundaries[elf][0] = Integer.valueOf(sections[0]);
-                boundaries[elf++][1] = Integer.valueOf(sections[1]);
-            }
-            Set<Integer> firstElf = IntStream.rangeClosed(boundaries[0][0], boundaries[0][1]).boxed().collect(Collectors.toSet());
-            Set<Integer> secondElf = IntStream.rangeClosed(boundaries[1][0], boundaries[1][1]).boxed().collect(Collectors.toSet());
-
-            total = setOperation.applyStrategy(total, firstElf, secondElf);
-        }
+        Arrays.stream(pairs).forEach(pair -> evaluatePair(setOperation, pair));
         return total;
+    }
+
+    private static void evaluatePair(Day4Strategy setOperation, String pair) {
+        String[] sectionRanges = pair.split(",");
+        int[][] boundaries = new int[2][2];
+        IntStream.range(0, sectionRanges.length).forEach(i -> assignBoundaries(sectionRanges, boundaries, i));
+        Set<Integer> firstElf = createSetOfSectionsForElf(boundaries[0]);
+        Set<Integer> secondElf = createSetOfSectionsForElf(boundaries[1]);
+        total = setOperation.applyStrategy(total, firstElf, secondElf);
+    }
+
+    private static Set<Integer> createSetOfSectionsForElf(int[] boundaries) {
+        return IntStream.rangeClosed(boundaries[0], boundaries[1]).boxed().collect(Collectors.toSet());
+    }
+
+    private static void assignBoundaries(String[] sectionRanges, int[][] boundaries, int i) {
+        String[] sections = sectionRanges[i].split("-");
+        boundaries[i][0] = Integer.parseInt(sections[0]);
+        boundaries[i][1] = Integer.parseInt(sections[1]);
     }
 
     private static class SecondPartDay4Strategy implements Day4Strategy {
